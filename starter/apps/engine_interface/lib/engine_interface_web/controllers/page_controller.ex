c054cb202ef872ed9a2f515193a572b4c69fdbda
Stefan Tennigkeit <stefan.tennigkeit@googlemail.com>
2018-08-14 10:14:46 +0200

    add a way to create new counter process

--- starter/apps/engine_interface/lib/engine_interface_web/controllers/page_controller.ex
+++ starter/apps/engine_interface/lib/engine_interface_web/controllers/page_controller.ex
@@ -1,7 +1,17 @@
 defmodule EngineInterfaceWeb.PageController do
   use EngineInterfaceWeb, :controller
 
+  alias Engine.CounterSupervisor
+
   def index(conn, _params) do
-    render conn, "index.html"
+    render(conn, "index.html")
+  end
+
+  def test(conn, %{"name" => name, "count" => count}) do
+    {:ok, _pid} = CounterSupervisor.start_counter(name, String.to_integer(count))
+
+    conn
+    |> put_flash(:info, "You entered the name: " <> name <> " and count of " <> count)
+    |> render("index.html")
   end
 end
--- starter/apps/engine_interface/lib/engine_interface_web/router.ex
+++ starter/apps/engine_interface/lib/engine_interface_web/router.ex
@@ -2,21 +2,23 @@ defmodule EngineInterfaceWeb.Router do
   use EngineInterfaceWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", EngineInterfaceWeb do
-    pipe_through :browser # Use the default browser stack
+    # Use the default browser stack
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    get("/", PageController, :index)
+    post("/test", PageController, :test)
   end
 
   # Other scopes may use custom stacks.
