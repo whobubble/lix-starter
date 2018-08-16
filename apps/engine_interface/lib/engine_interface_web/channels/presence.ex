b12fa9d028d1661662fd784cc5444dff1c7e630d
Stefan Tennigkeit <stefan.tennigkeit@googlemail.com>
2018-08-16 13:48:18 +0200

    dialyzer warning

--- apps/engine_interface/lib/engine_interface_web/channels/presence.ex
+++ apps/engine_interface/lib/engine_interface_web/channels/presence.ex
@@ -1,5 +1,6 @@
 defmodule EngineInterfaceWeb.Presence do
   @moduledoc false
 
+  @dialyzer :no_behaviours
   use Phoenix.Presence, otp_app: :engine_interface, pubsub_server: EngineInterface.PubSub
 end
