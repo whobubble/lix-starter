67ceab33f736d848647d6825b858bac0473dcb0c
Stefan Tennigkeit <stefan.tennigkeit@googlemail.com>
2018-08-21 01:19:17 +0200

    add config and test

--- web-client/tests/e2e/specs/test.js
+++ web-client/tests/e2e/specs/test.js
@@ -1,8 +1,23 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe("My First Test", () => {
+describe("Counter Engine", () => {
   it("Visits the app root url", () => {
     cy.visit("/");
     cy.contains("h1", "Welcome to Your Vue.js App");
+  });
+  it("Should open counter page", () => {
+    cy.get('[href="#/counters"]').click();
+    cy.contains("h1", "This is a page with counters");
+  });
+  it("Should create counter asdf", () => {
+    cy.get('[href="#/counters"]').click();
+    cy.get("input").type("asdf");
+    cy.get("button").click();
+    cy.contains("Counter asdf");
+    cy.get("span > div > input").should("have.value", "3");
+    cy.get("span > div > :nth-child(3)").click();
+    cy.get("span > div > input").should("have.value", "4");
+    cy.get("span > div > :nth-child(1)").click();
+    cy.get("span > div > input").should("have.value", "3");
   });
 });
